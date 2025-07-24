e7c437bcf464698b51b71fedc7cbdb5a6590d4c5
TaeyoungJang <nalgozo@naver.com>
2025-07-24 15:06:14 +0900

    검색기능 없는 프로젝트 삭제

--- HR_project/HRlist/HRapp/admin.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from .models import User
-
-class UserAdmin(admin.ModelAdmin):
-	list_display = ('user_name', 'user_birthdate', 'user_email' ,'user_phone')
-
-admin.site.register(User, UserAdmin)
